367c4e591f1408997aa12237217dfe6a0838cfe2
JaeHyeon <ljh040513@naver.com>
2021-02-11 16:48:17 +0900

    [ADD] TabBar example ADD
    ADD - Doit 안드로이드 chapter5 TabBar 예제 add

---
+++ Part2/chapter5/SampleTab/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+
+    Fragment1 fragment1;
+    Fragment2 fragment2;
+    Fragment3 fragment3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("통화기록"));
+        tabs.addTab(tabs.newTab().setText("스팸기록"));
+        tabs.addTab(tabs.newTab().setText("연락처"));
+
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Log.e("MainActivity", "선택된 탭 " + position);
+
+                Fragment selection = null;
+                if (position == 0) {
+                    selection = fragment1;
+                } else if (position == 1) {
+                    selection = fragment2;
+                } else if (position == 2) {
+                    selection = fragment3;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, selection).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+}
